bf3086694628cb97ec82f223d337fba86798ea6b
Noeël <noeelmoeskops@hotmail.com>
2019-07-17 18:47:14 +0200

    added Aruna lib

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
+#include <aruna.h>
+#include <pthread.h>
 
+using namespace aruna;
 int main() {
+    pthread_t aruna_FreeRTOSKernel;
+    pthread_create(&aruna_FreeRTOSKernel, NULL, vStartFreeRTOSKernel, NULL);
+
     std::cout << "Hello, World!" << std::endl;
+    printf("comm start: 0x%X\n", comm::start());
     return 0;
 }
